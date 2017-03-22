522fe718ec1af3c5b86f8137ecabf657f7ab1c94
Zerong Tony Wang <ztony_wang@hotmail.com>
2017-03-22 20:56:39 +1000

    added redirect from home

--- resume/urls.py
+++ resume/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from .views import ResumeView
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='resume'), name='index'),
     url(r'^resume/$', ResumeView.as_view(), name='resume')
 ]
